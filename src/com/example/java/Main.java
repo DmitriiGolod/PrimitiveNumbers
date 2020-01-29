32713a99df534d45325be91ad0c7ad637b7c55ce
Dmitrii_Golod <dmitry.golod@gmail.com>
2020-01-29 15:23:03 +0300

    Initial version (primitive types)

---
+++ src/com/example/java/Main.java
@@ -0,0 +1,17 @@
+package com.example.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+        byte b = 1;
+        short sh = 1;
+        int i = 1;
+        long l = 1L;
+
+        float f = 1f;
+        double d = 1d;
+
+        System.out.println("Byte: " + b);
+    }
+
+}
---
+++ src/com/example/java/MaxValues.java
@@ -0,0 +1,13 @@
+package com.example.java;
+
+public class MaxValues {
+
+    public static void main(String[] args) {
+        byte b = 127;
+        System.out.println("Byte: "+ b);
+        b++;
+       
+        System.out.println("Byte: "+ b);
+    }
+
+}
